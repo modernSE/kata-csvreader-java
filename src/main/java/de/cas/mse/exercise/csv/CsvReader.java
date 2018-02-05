a75e76cd90ecdcdb2dbe8d66c34720c01a7d6be7
Ferdinand Szekeresch <ferdinand.szekeresch@cas.de>
2018-02-05 22:43:17 +0100

    ferdi feature 3

--- src/main/java/de/cas/mse/exercise/csv/CsvReader.java
+++ src/main/java/de/cas/mse/exercise/csv/CsvReader.java
@@ -4,14 +4,21 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import de.cas.mse.exercise.csv.ui.CsvUi;
+import de.cas.mse.exercise.csv.ui.Filterable;
 
-public class CsvReader {
+public class CsvReader implements Filterable {
 	
 	private CsvUi csvUi;
 
 	private static final String DELIMETER = ",";
+
+	private List<List<String>> lines = new LinkedList<>();
 
 	public void run(final File csvFile) throws Exception {
 		// TODO: Start coding here
@@ -24,6 +31,7 @@ public class CsvReader {
 	private void addLine(String line) {
 		String[] splitLine = line.split(DELIMETER);
 		csvUi.addRow(Arrays.asList(splitLine));
+		lines.add(Arrays.asList(splitLine));
 	}
 
 	private void setUpUiSkeleton(String headLine) {
@@ -35,4 +43,15 @@ public class CsvReader {
 		this.csvUi = csvUi;
 	}
 
+	@Override
+	public List<List<String>> filter(int col, String word) {
+		return lines.stream().filter(l -> filterLine(l, col, word)).collect(Collectors.toList());
+	}
+
+	private boolean filterLine(List<String> l, int col, String word) {
+		if (word != null && !word.equals("")) {
+			return l.get(col-1).toLowerCase().contains(word.toLowerCase());
+		}
+		return true;
+	}
 }
