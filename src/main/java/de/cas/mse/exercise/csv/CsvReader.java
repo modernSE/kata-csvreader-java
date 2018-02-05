70fb10cba825aaa63803c60cfa93dba28e87a5c0
Ferdinand Szekeresch <ferdinand.szekeresch@cas.de>
2018-02-05 22:08:54 +0100

    ferdi feature 1+2

--- src/main/java/de/cas/mse/exercise/csv/CsvReader.java
+++ src/main/java/de/cas/mse/exercise/csv/CsvReader.java
@@ -1,6 +1,9 @@
 package de.cas.mse.exercise.csv;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.util.Arrays;
 
 import de.cas.mse.exercise.csv.ui.CsvUi;
 
@@ -8,10 +11,26 @@ public class CsvReader {
 	
 	private CsvUi csvUi;
 
+	private static final String DELIMETER = ",";
+
 	public void run(final File csvFile) throws Exception {
 		// TODO: Start coding here
+		BufferedReader bufferedReader = new BufferedReader(new FileReader(csvFile));
+
+		bufferedReader.lines().limit(1).forEach(this::setUpUiSkeleton);
+		bufferedReader.lines().skip(1).forEach(this::addLine);
 	}
-	
+
+	private void addLine(String line) {
+		String[] splitLine = line.split(DELIMETER);
+		csvUi.addRow(Arrays.asList(splitLine));
+	}
+
+	private void setUpUiSkeleton(String headLine) {
+		String[] splitLine = headLine.split(DELIMETER);
+		Arrays.stream(splitLine).forEach(csvUi::addColumn);
+	}
+
 	public void setCsvUi(final CsvUi csvUi) {
 		this.csvUi = csvUi;
 	}
