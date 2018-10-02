d7337e88ac894e7b519bb0537b4aa7768091d7d0
modernSE Trainee 5 <keydon+mse-trainee@gmail.com>
2018-10-02 16:52:11 +0200

    Ergebnis der Gruppe 5

--- src/main/java/de/cas/mse/exercise/csv/CsvReader.java
+++ src/main/java/de/cas/mse/exercise/csv/CsvReader.java
@@ -1,17 +1,32 @@
 package de.cas.mse.exercise.csv;
 
 import java.io.File;
+import java.nio.file.Files;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import de.cas.mse.exercise.csv.ui.CsvUi;
 
 public class CsvReader {
-	
+
 	private CsvUi csvUi;
 
 	public void run(final File csvFile) throws Exception {
-		// TODO: Start coding here
+		if (csvFile.exists() && csvFile.isFile()) {
+			List<String> lines = Files.readAllLines(csvFile.toPath());
+			List<List<String>> csvOutput = lines.stream().map((line) -> line.split(",")).map(Arrays::asList)
+					.collect(Collectors.toList());
+
+			csvUi.setColumnCount(csvOutput.get(0).size());
+
+			for (int i = 0; i < csvOutput.size(); i++) {
+				csvUi.addRow(csvOutput.get(i));
+			}
+
+		}
 	}
-	
+
 	public void setCsvUi(final CsvUi csvUi) {
 		this.csvUi = csvUi;
 	}
