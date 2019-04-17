1b88e19f2eaeb0c0a4a9f5e9cefcfb72efa33754
modernSE Trainee 3 <keydon+mse-trainee@gmail.com>
2019-04-17 15:55:13 +0200

    Ergebnis der Gruppe 3

--- src/main/java/de/cas/mse/exercise/csv/CsvReader.java
+++ src/main/java/de/cas/mse/exercise/csv/CsvReader.java
@@ -1,19 +1,58 @@
 package de.cas.mse.exercise.csv;
 
+
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import de.cas.mse.exercise.csv.ui.CsvUi;
+import de.cas.mse.exercise.csv.ui.Filterable;
 
-public class CsvReader {
+public class CsvReader implements Filterable {
 	
 	private CsvUi csvUi;
+	
+	private List<List<String>> data = new ArrayList<>();
+	
 
 	public void run(final File csvFile) throws Exception {
-		// TODO: Start coding here
+	 
+	    BufferedReader csvReader=new BufferedReader(new FileReader(csvFile));
+		String line=csvReader.readLine();
+		List<String> headerRow = Arrays.asList(line.split(","));
+		for (String columnName : headerRow) {
+			csvUi.addColumn(columnName);
+		}
+
+		line=csvReader.readLine();
+		while (line!=null) {
+			List<String> csvRow = Arrays.asList(line.split(","));
+			csvUi.addRow(csvRow);
+			data.add(csvRow);
+			line=csvReader.readLine();
+		}
+		csvReader.close();
+				
 	}
 	
 	public void setCsvUi(final CsvUi csvUi) {
 		this.csvUi = csvUi;
+	}
+
+	@Override
+	public List<List<String>> filter(int col, String word) {
+		
+		List<List<String>> result = new ArrayList<>();
+		
+		for(List<String> row: data) {
+			if (row.get(col - 1).toLowerCase().contains(word.toLowerCase())){
+				result.add(row);
+			}
+		}
+		return result;
 	}
 
 }
