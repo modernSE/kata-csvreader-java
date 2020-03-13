3d570a1c9cd1113be37d705cfdaafbe0e7d1d47e
modernSE Trainee 4 <keydon+mse-trainee@gmail.com>
2020-03-13 16:05:25 +0100

    Ergebnis der Gruppe 4

--- src/main/java/de/cas/mse/exercise/csv/CsvReader.java
+++ src/main/java/de/cas/mse/exercise/csv/CsvReader.java
@@ -1,17 +1,52 @@
 package de.cas.mse.exercise.csv;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 
 import de.cas.mse.exercise.csv.ui.CsvUi;
 
 public class CsvReader {
-	
+
 	private CsvUi csvUi;
 
 	public void run(final File csvFile) throws Exception {
-		// TODO: Start coding here
+		List<List<String>> csvRows = readCSV(csvFile, ",");
+		
+		List<String> csvHeader = csvRows.get(0);
+		addHeaderCaptions(csvHeader);
+		
+		List<List<String>> csvContents = csvRows.subList(1, csvRows.size());
+		addRowContents(csvContents);
 	}
-	
+
+	private List<List<String>> readCSV(File csvFile, final String delimiter) throws FileNotFoundException {
+		List<List<String>> rows = new ArrayList<>();
+		try (Scanner fileScanner = new Scanner(csvFile)) {
+			while (fileScanner.hasNext()) {
+				String row = fileScanner.nextLine();
+				List<String> rowCells = Arrays.asList(row.split(delimiter));
+				rows.add(rowCells);
+			}
+		}
+		return rows;
+	}
+
+	private void addRowContents(List<List<String>> csvRows) {
+		for (List<String> row : csvRows) {
+			csvUi.addRow(row);
+		}
+	}
+
+	private void addHeaderCaptions(List<String> headerCaptions) {
+		for (String caption : headerCaptions) {
+			csvUi.addColumn(caption);
+		}
+	}
+
 	public void setCsvUi(final CsvUi csvUi) {
 		this.csvUi = csvUi;
 	}
