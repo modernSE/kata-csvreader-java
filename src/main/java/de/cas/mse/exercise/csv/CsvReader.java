4df093b8b2e2ecae2572e833919633c0e111d977
Johannes Wachter <Johannes.Wachter@cas.de>
2020-07-03 11:19:57 +0200

    step 1
    Change-Id: I49efdfc43b80fa7bb9724ec035d6ee2c5e2a90e2

--- src/main/java/de/cas/mse/exercise/csv/CsvReader.java
+++ src/main/java/de/cas/mse/exercise/csv/CsvReader.java
@@ -1,17 +1,37 @@
 package de.cas.mse.exercise.csv;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.util.Arrays;
 
 import de.cas.mse.exercise.csv.ui.CsvUi;
 
 public class CsvReader {
-	
+
+	private static final String COMMA_SEPARATOR = ",";
+
 	private CsvUi csvUi;
 
 	public void run(final File csvFile) throws Exception {
-		// TODO: Start coding here
+		FileReader fileInputStream = new FileReader(csvFile);
+
+		try(BufferedReader bufferedReader = new BufferedReader(fileInputStream)) {
+
+			int colCount = -1;
+			String s = null;
+			while ((s = bufferedReader.readLine()) != null) {
+				String[] split = s.split(COMMA_SEPARATOR);
+				if (colCount < 0) {
+					colCount = split.length;
+					csvUi.setColumnCount(colCount);
+				}
+				csvUi.addRow(Arrays.asList(split));
+			}
+		}
+
 	}
-	
+
 	public void setCsvUi(final CsvUi csvUi) {
 		this.csvUi = csvUi;
 	}
