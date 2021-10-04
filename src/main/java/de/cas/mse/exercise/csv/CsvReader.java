f4cd9d4df3b56d6ee40cc4dc648ccc9fa5f4e4f8
Lukas Bugaj <lukas.bugaj@cas.de>
2021-10-04 11:22:50 +0200

    Ergebnis Mob Programming

--- src/main/java/de/cas/mse/exercise/csv/CsvReader.java
+++ src/main/java/de/cas/mse/exercise/csv/CsvReader.java
@@ -1,6 +1,11 @@
 package de.cas.mse.exercise.csv;
 
 import java.io.File;
+import java.nio.file.Files;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import de.cas.mse.exercise.csv.ui.CsvUi;
 
@@ -9,9 +14,23 @@ public class CsvReader {
 	private CsvUi csvUi;
 
 	public void run(final File csvFile) throws Exception {
-		// TODO: Start coding here
+		
+		List<String> allRows = Files.readAllLines(csvFile.toPath());
+		List<String> columnNames = parseRowOrHeader(allRows.get(0));
+		Stream<String> dataRows = allRows.stream().skip(1);
+
+		columnNames
+			.forEach(csvUi::addColumn);
+		dataRows
+			.map(this::parseRowOrHeader)
+			.forEach(csvUi::addRow);
 	}
-	
+
+	private List<String> parseRowOrHeader(String row) {
+		List<String> columns = Arrays.asList(row.split(","));
+		return columns;
+	}
+
 	public void setCsvUi(final CsvUi csvUi) {
 		this.csvUi = csvUi;
 	}
