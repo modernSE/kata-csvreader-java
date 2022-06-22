2d2834ce3f448215e1734cc59ae91cbaaec304ed
Lukas Bugaj <lukas.bugaj@cas.de>
2022-06-22 13:20:10 +0200

    ergebnis mob 22.06.2022

--- src/main/java/de/cas/mse/exercise/csv/CsvReader.java
+++ src/main/java/de/cas/mse/exercise/csv/CsvReader.java
@@ -1,6 +1,11 @@
 package de.cas.mse.exercise.csv;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Arrays;
+import java.util.List;
 
 import de.cas.mse.exercise.csv.ui.CsvUi;
 
@@ -10,6 +15,26 @@ public class CsvReader {
 
 	public void run(final File csvFile) throws Exception {
 		// TODO: Start coding here
+		BufferedReader reader = readFile(csvFile);
+
+		String line = reader.readLine();
+		List<String> row = parseCells(line);
+		csvUi.addRow(row);
+		csvUi.setColumnCount(row.size());
+		
+		while((line = reader.readLine()) != null) {
+			row = parseCells(line);
+			csvUi.addRow(row);
+		}
+		
+	}
+
+	private BufferedReader readFile(final File csvFile) throws FileNotFoundException {
+		return new BufferedReader(new FileReader(csvFile));
+	}
+
+	private List<String> parseCells(String line) {
+		return Arrays.asList(line.split(","));
 	}
 	
 	public void setCsvUi(final CsvUi csvUi) {
