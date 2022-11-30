f1e3bbab7554580a930c9ab021577961190f765c
Johannes Wachter <me@jwachter.de>
2022-11-30 13:32:17 +0000

    Solution 1

--- src/main/java/de/cas/mse/exercise/csv/CsvReader.java
+++ src/main/java/de/cas/mse/exercise/csv/CsvReader.java
@@ -1,6 +1,9 @@
 package de.cas.mse.exercise.csv;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.List;
 
 import de.cas.mse.exercise.csv.ui.CsvUi;
 
@@ -8,12 +11,40 @@ public class CsvReader {
 	
 	private CsvUi csvUi;
 
-	public void run(final File csvFile) throws Exception {
-		// TODO: Start coding here
+	public void run(File file) throws Exception {
+
+		var rows = parseRows(file);
+
+		rows.stream()
+			.findFirst()
+			.map(CsvRow::columnCount)
+			.ifPresent(csvUi::setColumnCount);
+		
+		rows.stream()
+			.map(CsvRow::values)
+			.forEach(csvUi::addRow);
+	}
+
+	private List<CsvRow> parseRows(File file) throws IOException {
+		return Files
+			.lines(file.toPath())
+			.map(this::parseLine)
+			.map(CsvRow::new)
+			.toList();
+	}
+
+	private List<String> parseLine(String line) {
+		return List.of(line.split(","));
 	}
 	
-	public void setCsvUi(final CsvUi csvUi) {
+	public void setCsvUi(CsvUi csvUi) {
 		this.csvUi = csvUi;
+	}
+
+	private record CsvRow(List<String> values) {
+		int columnCount() {
+			return values.size();
+		}
 	}
 
 }
